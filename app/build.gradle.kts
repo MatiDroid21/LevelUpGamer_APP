d077220927146c0fa70a6934151e9f0c9fa343d1
MatiDroid12 <matias.chavez711@gmail.com>
2025-11-19 19:45:36 -0300

    nuevas dependencias para empezar a consumir apis y además de agregar tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,4 +89,72 @@ dependencies {
     // optional - Paging 3 Integration
     implementation("androidx.room:room-paging:$room_version")
 
+    //Configuración para test unitarios
+    // Kotest framework de testing para Kotlin
+    testImplementation("io.kotest:kotest-runner-junit5:5.8.0") // Permite ejecutar los tests de Kotest en JUnit 5, porque Android Studio por defecto usa JUnit.
+    testImplementation("io.kotest:kotest-assertions-core:5.8.0") // Asserts -> verificaciones
+
+    // JUnit 5
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0") // JUnit (JUnit 5), el estándar de pruebas en Java/Kotlin.
+
+    // MockK
+    testImplementation("io.mockk:mockk:1.13.10") // librería para crear: mocks (objetos falsos), stubs (respuestas falsas), spies, verificación de llamadas
+
+    // Compose UI Test
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.2") // Permite renderizar Composables en tests, buscar nodos en la UI, hacer clicks, validar textos, correr en un emulador real
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.2") // Necesario para que el entorno de pruebas tenga actividad
+
+    // Coroutines testing
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1") // Testear funciones suspend {} sin depender de delays reales.
+
+
+    // implements del profe}
+
+    ksp("androidx.room:room-compiler:$room_version")
+
+    // If this project only uses Java source, use the Java annotationProcessor
+    // No additional plugins are necessary
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+
+    // optional - RxJava2 support for Room
+    implementation("androidx.room:room-rxjava2:$room_version")
+
+    // optional - RxJava3 support for Room
+    implementation("androidx.room:room-rxjava3:$room_version")
+
+    // optional - Guava support for Room, including Optional and ListenableFuture
+    implementation("androidx.room:room-guava:$room_version")
+
+    // optional - Test helpers
+    testImplementation("androidx.room:room-testing:$room_version")
+
+    // optional - Paging 3 Integration
+    implementation("androidx.room:room-paging:$room_version")
+
+    // Para implementar consumo de API
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    // Coroutines + lifecycle
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+
+    // Moshi
+    implementation("com.squareup.retrofit2:converter-moshi:2.11.0")
+
+    // OkHttp
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    // Coil (si quieres previsualizar imagenes en Compose)
+    implementation("io.coil-kt:coil-compose:2.6.0")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
